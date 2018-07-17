35d3b9d3a124f34eb1bd087f578a7e7b849edd11
Casey Schneebeck <cschneebeck@tnc.org>
2018-07-17 11:44:05 -0600

    starter code

---
+++ js/clicks.js
@@ -0,0 +1,20 @@
+define([
+	"dojo/_base/declare", "esri/tasks/query", "esri/tasks/QueryTask", "esri/graphicsUtils"
+],
+function ( declare, Query, QueryTask, graphicsUtils ) {
+        "use strict";
+
+        return declare(null, {
+			eventListeners: function(t){
+				$("#" + t.id + "explorerSelect").chosen({allow_single_deselect:false, width:"98%"})
+					.change(function(c){
+						console.log(c.target.value);
+						t.map.setMapCursor("pointer");
+					});	
+			},
+			numberWithCommas: function(x){
+				return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+			}
+        });
+    }
+);
